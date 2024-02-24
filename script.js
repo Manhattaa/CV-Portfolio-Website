ca7ec639a8020b3960e25f2a649067a7a894ae0d
Fady Hatta <Fadynhatta@gmail.com>
2024-02-24 13:43:08 +0100

    removed some elements that created some issues with the pathing

--- script.js
+++ script.js
@@ -1,75 +1,83 @@
 function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
-
-// Function to close menu when clicking outside
-document.addEventListener("click", function(event) {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  // Check if the clicked element is not part of the menu or the hamburger icon and the menu is open
-  if (!menu.contains(event.target) && !icon.contains(event.target) && menu.classList.contains("open")) {
-      toggleMenu(); // If so, close the menu
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
   }
-});
-
-// Dark / light mode
-const btn = document.getElementById("modeToggle");
-const btn2 = document.getElementById("modeToggle2");
-const themeIcons = document.querySelectorAll(".icon");
-const currentTheme = localStorage.getItem("theme");
-
-if (currentTheme === "dark") {
-  setDarkMode();
-}
-
-btn.addEventListener("click", function() {
-  setTheme();
-});
-
-btn2.addEventListener("click", function() {
-  setTheme();
-});
-
-function setTheme() {
-  let currentTheme = document.body.getAttribute("theme");
-
+  
+  // Function to close menu when clicking outside
+  document.addEventListener("click", function(event) {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    // Check if the clicked element is not part of the menu or the hamburger icon and the menu is open
+    if (!menu.contains(event.target) && !icon.contains(event.target) && menu.classList.contains("open")) {
+        toggleMenu(); // If so, close the menu
+    }
+  });
+  
+  // Dark / light mode
+  const btn = document.getElementById("modeToggle");
+  const btn2 = document.getElementById("modeToggle2");
+  const themeIcons = document.querySelectorAll(".icon");
+  const currentTheme = localStorage.getItem("theme");
+  
   if (currentTheme === "dark") {
-      setLightMode();
-  } else {
-      setDarkMode();
+    setDarkMode();
   }
-}
-
-function setDarkMode() {
-  document.body.setAttribute("theme", "dark");
-  localStorage.setItem("theme", "dark");
-
-  themeIcons.forEach((icon) => {
-      icon.src = icon.getAttribute("src-dark");
+  
+  btn.addEventListener("click", function() {
+    setTheme();
   });
-}
-
-function setLightMode() {
-  document.body.removeAttribute("theme");
-  localStorage.setItem("theme", "light");
-
-  themeIcons.forEach((icon) => {
-      icon.src = icon.getAttribute("src-light");
+  
+  btn2.addEventListener("click", function() {
+    setTheme();
   });
-}
-
-
-
-var profilePic = document.getElementById("profilePic");
-var profileElements = document.querySelectorAll(".profile-element");
-
-/* document.querySelector('a[href="#2048"]').addEventListener("click", function(event) {
-  event.preventDefault(); 
-  var game = new Game(4);
-  game.setup();
-}); */
-
-
+  
+  function setTheme() {
+    let currentTheme = document.body.getAttribute("theme");
+  
+    if (currentTheme === "dark") {
+        setLightMode();
+    } else {
+        setDarkMode();
+    }
+  }
+  
+  function setDarkMode() {
+    document.body.setAttribute("theme", "dark");
+    localStorage.setItem("theme", "dark");
+  
+    themeIcons.forEach((icon) => {
+        icon.src = icon.getAttribute("src-dark");
+    });
+  }
+  
+  function setLightMode() {
+    document.body.removeAttribute("theme");
+    localStorage.setItem("theme", "light");
+  
+    themeIcons.forEach((icon) => {
+        icon.src = icon.getAttribute("src-light");
+    });
+  }
+  
+  
+  
+  var profilePic = document.getElementById("profilePic");
+  var profileElements = document.querySelectorAll(".profile-element");
+  
+  /* document.querySelector('a[href="#2048"]').addEventListener("click", function(event) {
+    event.preventDefault(); 
+    var game = new Game(4);
+    game.setup();
+  }); */
+  
+      document.getElementById("scrollToTopBtn").addEventListener("click", function() {
+          window.scrollTo({
+              top: 0,
+              behavior: "smooth"
+          });
+      });
+  
+  
+  
