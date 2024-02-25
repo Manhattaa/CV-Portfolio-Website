90901359fd53855b7230756fa1418e51192473ce
Fady Hatta <Fadynhatta@gmail.com>
2024-02-25 04:19:04 +0100

    new song scripts as well as the song itself!

--- script.js
+++ script.js
@@ -1,62 +1,73 @@
 document.addEventListener("DOMContentLoaded", function() {
   const floatingPicContainer = document.querySelector(".floating__pic-container");
   const astronauts = document.querySelectorAll('.astronaut');
+  const backgroundMusic = document.getElementById('backgroundMusic');
 
-  // Hide astronauts initially
+  // hide astronauts initially
   astronauts.forEach(function(astronaut) {
-      astronaut.style.opacity = 0;
-      astronaut.style.pointerEvents = 'none';
+    astronaut.style.opacity = 0;
+    astronaut.style.pointerEvents = 'none';
   });
 
-  floatingPicContainer.addEventListener('click', function() {
-      // Show astronauts upon click
-      astronauts.forEach(function(astronaut) {
-          astronaut.style.opacity = 1;
-          astronaut.style.pointerEvents = 'auto';
-      });
+  // function to handle click event on floating pic container
+  function handleClick() {
+    // show astronauts upon click
+    astronauts.forEach(function(astronaut) {
+      astronaut.style.opacity = 1;
+      astronaut.style.pointerEvents = 'auto';
+    });
+
+    // play background music only once
+    backgroundMusic.play();
+    
+    // remove the event listener after playing the music once
+    floatingPicContainer.removeEventListener('click', handleClick);
+
+    // logic to animate astronauts
+    astronauts.forEach((astronaut, index) => {
+      let randomTop, randomLeft, randomRotate;
+
+      if (index === 0) { // 1st astronaut = bottom to top
+        randomTop = window.innerHeight + 100; // starting from the bottom of the page
+        randomLeft = Math.random() * (window.innerWidth - astronaut.clientWidth);
+        randomRotate = Math.random() * 360; // random initial movement
+        astronaut.style.transform = 'rotate(' + randomRotate + 'deg)';
+        setTimeout(() => {
+          astronaut.style.transitionDuration = '16s'; // Longer animation duration
+          astronaut.style.transform = 'translateY(-' + (window.innerHeight + 100) + 'px) rotate(' + randomRotate + 'deg)';
+        }, 1000); // Delay for CSS to start
+      } else if (index === 1) { // 2nd astronaut = right to left
+        randomTop = Math.random() * (window.innerHeight - astronaut.clientHeight);
+        randomLeft = window.innerWidth + 100; // starting from right side of the page
+        randomRotate = Math.random() * 360; // random initial movement
+        astronaut.style.transform = 'rotate(' + randomRotate + 'deg)';
+        setTimeout(() => {
+          astronaut.style.transitionDuration = '16s'; // Longer animation duration
+          astronaut.style.transform = 'translateX(-' + (window.innerWidth + 100) + 'px) rotate(' + randomRotate + 'deg)';
+        }, 1000); // Delay for CSS to start
+      } else { // 3rd astronaut = left to right
+        randomTop = Math.random() * (window.innerHeight - astronaut.clientHeight);
+        randomLeft = -100 - astronaut.clientWidth; // starting from left side
+        randomRotate = Math.random() * 360; // random initial movement
+        astronaut.style.transform = 'rotate(' + randomRotate + 'deg)';
+        setTimeout(() => {
+          astronaut.style.transitionDuration = '16s'; // Longer animation duration
+          astronaut.style.transform = 'translateX(' + (window.innerWidth + 100) + 'px) rotate(' + randomRotate + 'deg)';
+        }, 1000); // Delay for CSS to start
+      }
+
+      astronaut.style.top = randomTop + 'px';
+      astronaut.style.left = randomLeft + 'px';
       
-      // Astronaut animation code remains unchanged
-      astronauts.forEach((astronaut, index) => {
-          let randomTop, randomLeft, randomRotate;
-
-          if (index === 0) { //1st astronaut = bottom to top
-              randomTop = window.innerHeight + 100; // starting from the bottom of the page!!
-              randomLeft = Math.random() * (window.innerWidth - astronaut.clientWidth);
-              randomRotate = Math.random() * 360; // random initial movement
-              astronaut.style.transform = 'rotate(' + randomRotate + 'deg)';
-              setTimeout(() => {
-                  astronaut.style.transitionDuration = '20s'; // Longer animation duration
-                  astronaut.style.transform = 'translateY(-' + (window.innerHeight + 100) + 'px) rotate(' + randomRotate + 'deg)';
-              }, 1000); // + delay!!
-          } else if (index === 1) { // 2nd astronaut = right to left
-              randomTop = Math.random() * (window.innerHeight - astronaut.clientHeight);
-              randomLeft = window.innerWidth + 100; // starting from right side of the page
-              randomRotate = Math.random() * 360; // random initial movement
-              astronaut.style.transform = 'rotate(' + randomRotate + 'deg)';
-              setTimeout(() => {
-                  astronaut.style.transitionDuration = '20s'; // longer animation time
-                  astronaut.style.transform = 'translateX(-' + (window.innerWidth + 100) + 'px) rotate(' + randomRotate + 'deg)';
-              }, 1000); // delay for css
-          } else { // 3rd astronaut = left to right
-              randomTop = Math.random() * (window.innerHeight - astronaut.clientHeight);
-              randomLeft = -100 - astronaut.clientWidth; // starting from left side
-              randomRotate = Math.random() * 360; // random initial movement
-              astronaut.style.transform = 'rotate(' + randomRotate + 'deg)';
-              setTimeout(() => {
-                  astronaut.style.transitionDuration = '20s'; // Longer animation duration
-                  astronaut.style.transform = 'translateX(' + (window.innerWidth + 100) + 'px) rotate(' + randomRotate + 'deg)';
-              }, 1000); // delay for css to start
-          }
-
-          astronaut.style.top = randomTop + 'px';
-          astronaut.style.left = randomLeft + 'px';
-          
-          // removes astronaut when it finishes its spacewalk. dont worry no astronaut was harmed  in the making of this website!
-          astronaut.addEventListener('transitionend', function() {
-              this.remove();
-          });
+      // Remove the astronaut when it finishes its spacewalk
+      astronaut.addEventListener('transitionend', function() {
+        this.remove();
       });
-  });
+    });
+  }
+
+  // Add click event listener to floating pic container
+  floatingPicContainer.addEventListener('click', handleClick);
 });
 
 function toggleMenu() {
